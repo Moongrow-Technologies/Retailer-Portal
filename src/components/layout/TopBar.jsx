9a84b0c7512ce1b227eea1745fedbb56c87e15a6
Ahmet Uras <xahmetux@gmail.com>
2026-04-15 17:52:47 +0000

    File changes

--- src/components/layout/TopBar.jsx
+++ src/components/layout/TopBar.jsx
@@ -84,7 +84,7 @@ export default function TopBar() {
       </div>
 
       {/* Right side */}
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-4">
         <div className="relative">
           <button
             id="help-button"
