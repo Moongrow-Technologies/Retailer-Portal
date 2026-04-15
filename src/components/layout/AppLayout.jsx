b52e6c49dabd0e96f68f30ac757e1fafd9086c55
Taylor Wilson <tawilson@tcd.ie>
2026-04-15 16:41:43 +0000

    Visual edits

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -9,7 +9,7 @@ export default function AppLayout() {
       <Sidebar />
       <TopBar />
       <main className="ml-[220px] pt-14 min-h-screen">
-        <div className="bg-[hsl(var(--secondary))] p-8">
+        <div className="bg-[hsl(var(--background))] p-8">
           <Outlet />
         </div>
       </main>
