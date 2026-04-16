2e6733109fb566f863facc95be01d9fb724cd216
Ahmet Uras <xahmetux@gmail.com>
2026-04-16 03:59:38 +0000

    File changes

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -29,7 +29,7 @@ export default function Sidebar() {
   return (
     <aside className="fixed left-0 top-14 h-[calc(100vh-56px)] w-[220px] bg-white border-r border-[#EBEBF0] flex flex-col z-40">
       {/* Navigation */}
-      <nav className="flex-1 pt-6 pb-4 px-5 space-y-1 overflow-y-auto">
+      <nav className="flex-1 pb-4 px-5 space-y-1 overflow-y-auto" style={{paddingTop: '27px'}}>
         {navItems.map((item) => {
           const active = isActive(item.path);
           return (
