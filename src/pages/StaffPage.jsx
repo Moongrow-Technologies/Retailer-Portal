06ca1e14d20b967d16dd7a07417cd06e72df01df
Ahmet Uras <xahmetux@gmail.com>
2026-04-16 06:23:34 +0000

    File changes

--- src/pages/StaffPage.jsx
+++ src/pages/StaffPage.jsx
@@ -21,7 +21,7 @@ export default function StaffPage() {
   };
 
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       {/* Header */}
       <div className="flex items-start justify-between mb-6">
         <div>
